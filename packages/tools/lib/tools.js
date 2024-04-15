a3c6b8d473de5e131fa345d499bba34c022b00bd
goodkeeper <guo2401143699@qq.com>
2024-04-15 17:50:09 +0800

    etst

--- packages/tools/lib/tools.js
+++ packages/tools/lib/tools.js
@@ -1,10 +1,13 @@
 'use strict';
 
 module.exports = tools;
-module.exports = toolsc;
+module.exports = toolsc111;
 function tools() {
   return 'Hello from lerna';
 }
 function toolsc(){
+  return 'Hello from toolsc';
+}
+function toolsc111(){
   return 'Hello from toolsc';
 }
